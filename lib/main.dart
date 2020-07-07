d074c95b26b0291cb48ab33149262e70c54366ad
Kevin Nguyen <khakha1512.nk@gmail.com>
2020-07-06 20:12:08 -0400

    Decimal and negative numbers will be added soon

--- lib/main.dart
+++ lib/main.dart
@@ -31,13 +31,17 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
   String displayString = "0";
+  String numberString = "0";
+  String operation;
+  double result = 0;
+  bool shouldCalculate = false, afterEqual = false;
 
   Widget createButton(String title) {
     return Expanded(
       child: ButtonTheme(
         height: double.infinity,
         child: OutlineButton(
-          onPressed: () {},
+          onPressed: () => pressButton(title),
           child: Text (
               title,
             style: TextStyle(
@@ -53,6 +57,77 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
     );
+  }
+
+  pressButton (String title) {
+    setState(() {
+      try {
+        if (title == '+' || title == '-' || title == '*' || title == '/') {
+          if (shouldCalculate) {
+            calculate();
+          } else {
+            result = double.parse(numberString);
+            shouldCalculate = true;
+          }
+          operation = title;
+          numberString = '';
+        } else if (title == "CE") {
+          displayString = "0";
+          numberString = "";
+          result = 0;
+          operation = "";
+          shouldCalculate = false;
+        } else if (title == "=") {
+          calculate();
+          shouldCalculate = false;
+          afterEqual = true;
+        } else {
+          if (numberString == "0" || numberString == '0.0') {
+            numberString = '';
+          }
+          if (afterEqual) {
+            afterEqual = false;
+            displayString = "";
+            numberString = "";
+          }
+          numberString += title;
+          displayString = numberString;
+        }
+      } on FormatException catch (e) {
+        operation = title;
+      }
+    });
+  }
+
+  calculate() {
+    switch (operation) {
+      case '+':
+        result += double.parse(numberString);
+        break;
+      case '-':
+        result -= double.parse(numberString);
+        break;
+      case '*':
+        result *= double.parse(numberString);
+        break;
+      case '/':
+        result /= double.parse(numberString);
+        break;
+      default:
+        displayString = "spec1";
+        break;
+    }
+    if (displayString == "spec1") {
+      displayString = numberString;
+      return;
+    }
+    if (result % 1 == 0) {
+      int temp = result.toInt();
+      numberString = temp.toString();
+    } else {
+      numberString = result.toString();
+    }
+    displayString = numberString;
   }
 
   Widget createRow(String name1, String name2, String name3, String name4) {
@@ -101,7 +176,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   createRow("+", "-", "*", "/"),
                   createRow("7", "8", "9", "0"),
                   createRow("4", "5", "6", "CE"),
-                  createRow("1", "2", "4", "0"),
+                  createRow("1", "2", "3", "="),
                 ],
               ),
             ),
